3b7a0cca1d5d059449766b34cb445775fb9aa75a
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2020-04-19 15:32:00 -0400

    added message event and command

---
+++ commands/System/ping.js
@@ -0,0 +1,8 @@
+exports.run = (bakery, msg) => {
+	msg.channel.send("Pong! `69ms`")
+}
+
+exports.help = {
+	aliases: [],
+	name: "ping"
+}
--- events/message.js
+++ events/message.js
@@ -1,6 +1,16 @@
 module.exports = (bakery, msg) => {
-	if(msg.author.bot) return;
-	if(msg.content === "hi") {
-		msg.channel.send("hello")
-	}
+    if (msg.channel.type === 'dm') return;
+    let prefix = "bakery ";
+    if (!msg.content.toLowerCase().startsWith(prefix) || msg.author.bots) return;
+    const args = msg.content.slice(prefix.length).split(/ +/);
+    const commandName = args.shift().toLowerCase();
+    const command = bakery.commands.get(commandName) || bakery.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+
+    if (!command) return;
+    
+    try {
+        return command.run(bakery, msg, args);
+    } catch (err) {
+        return console.error(err);
+    }
 }
