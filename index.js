f4315e700dd5da3e713dabd7fa8b59f6346c338e
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2020-04-19 15:07:38 -0400

    paste event loader

--- index.js
+++ index.js
@@ -20,4 +20,31 @@ fs.readdirSync(__dirname + "/../commands/").forEach((folder) => {
     })
 })
 
+try {
+    let files = fs.readdirSync("./events/")
+    files = files.filter(f => f.split(".").pop() === "js")
+    if(files.length === 0) {
+        console.log("There are no events to load.\n\n")
+        return;
+    }
+
+    let loadednum = 0
+    for(let i = 0; i < files.length; i++) {
+        const _event = files[i].slice(0, -3)
+        try {
+            const event = require(`./events/${files[i]}`)
+            client.on(files[i].slice(0, -3), event.bind(null, client))
+            console.log(`Successfully loaded event ${_event}.`)
+            loadednum++
+        } catch(err) {
+            const trace = err.stack.toString().split("\n").slice(0, 3).join("\n")
+            console.log(`An error occured while trying to load ${_event}\n${trace}`)
+            console.log(`Could not load the event ${_event}.`)
+        }
+    }
+    console.log(`Successfully loaded ${loadednum} events.\n`)
+} catch(err) {
+    console.log(err)
+}
+
 bakery.login(process.env.TOKEN)
