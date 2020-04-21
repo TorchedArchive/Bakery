135c0e3034710cc0467e731f84d9e15ae0cd1b6a
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2020-04-20 23:42:13 -0400

    changed how the balance works for first time users

--- commands/Economy/balance.js
+++ commands/Economy/balance.js
@@ -2,7 +2,17 @@ const Database = require('better-sqlite3');
 const db = new Database('././src/Bakery.db');
 
 exports.run = async (bakery, msg) => {
-	if(!bakery.economy.hasBakery(msg.author.id)) return bakery.economy.init(msg.author.id)
+	if(!bakery.economy.hasBakery(msg.author.id)) {
+		msg.channel.send({embed: {
+			color: 0xFA3764,
+			description: "Looks like you don't own a bakery! Start one with `bakery start`!"
+		}})
+	} else {
+		msg.channel.send({embed: {
+			color: 0xFB83C4,
+			description: "This command is still in testing."
+		}})
+	}
 }
 
 exports.help = {
