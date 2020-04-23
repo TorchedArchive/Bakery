63aaba99d6858186f48cc7c3e338e3b8e4175358
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2020-04-22 21:10:13 -0400

    remove constructor

--- src/Structures/Databases/EconomyFunctions.js
+++ src/Structures/Databases/EconomyFunctions.js
@@ -2,12 +2,11 @@ const Database = require('better-sqlite3');
 const db = new Database('././src/Bakery.db');
 
 class EconomyFunctions {
-	constructor() {}
 
 	static init(userID) {
 		const row = db.prepare("SELECT * FROM users WHERE id=?;").get(userID)
 		if(!row) { 
-			return db.prepare("INSERT INTO users (id, money, badges, daily) VALUES (?, ?, ?, ?);").run(userID, 0, "[]", 0)
+			return db.prepare("INSERT INTO users (id, money, badges, daily, optout) VALUES (?, ?, ?, ?, ?);").run(userID, 0, "[]", 0, 0)
 		} else {
 			return;
 		}
@@ -35,13 +34,13 @@ class EconomyFunctions {
 
 	static give(userID, amount) {
 		const row = db.prepare("SELECT * FROM users WHERE id=?;").get(userID)
-		db.prepare("REPLACE INTO users (id, money, badges, daily) VALUES (?, ?, ?, ?);").run(userID, amount, row.badges, row.daily)
+		db.prepare("REPLACE INTO users (id, money, badges, daily, optout) VALUES (?, ?, ?, ?, ?);").run(userID, amount, row.badges, row.daily, row.optout)
 		return;
 	}
 
 	static daily(userID) {
 		const row = db.prepare("SELECT * FROM users WHERE id=?;").get(userID)
-		db.prepare("REPLACE INTO users (id, money, badges, daily) VALUES (?, ?, ?, ?);").run(userID, row.money + 500, row.badges, Date.now())
+		db.prepare("REPLACE INTO users (id, money, badges, daily, optout) VALUES (?, ?, ?, ?, ?);").run(userID, row.money + 500, row.badges, Date.now(), row.optout)
 		return;
 	}
 }
